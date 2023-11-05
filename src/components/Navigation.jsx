87e3d5a320e2e982a8970df0eaf2ccf80373cb7f
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-11-05 11:31:36 -0600

    Changed css of nav items and footer items

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -15,30 +15,28 @@ export default function Navigation() {
       <ul className="nav nav-tabs">
         <li
           onClick={(e) => handlePageChange("about-me", e)}
-          className={activeItem === "about-me" ? "nav-link active" : "nav-link"}
+          className="navigation-item"
         >
           <Link to="/about-me" className={activeItem === "about-me" ? "chosen" : ""}>About Me</Link>
         </li>
 
         <li
           onClick={(e) => handlePageChange("portfolio", e)}
-          className={
-            activeItem === "portfolio" ? "nav-link active" : "nav-link"
-          }
+          className="navigation-item"
         >
           <Link to="/portfolio" className={activeItem === "portfolio" ? "chosen" : ""}>Portfolio</Link>
         </li>
 
         <li
           onClick={(e) => handlePageChange("contact", e)}
-          className={activeItem === "contact" ? "nav-link active" : "nav-link"}
+          className="navigation-item"
         >
           <Link to="/contact" className={activeItem === "contact" ? "chosen" : ""}>Contact</Link>
         </li>
 
         <li
           onClick={(e) => handlePageChange("resume", e)}
-          className={activeItem === "resume" ? "nav-link active" : "nav-link"}
+          className="navigation-item"
         >
           <Link to="/resume" className={activeItem === "resume" ? "chosen" : ""}>Resume</Link>
         </li>
