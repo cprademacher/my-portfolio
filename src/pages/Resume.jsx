a308369bf397951f6f3d56b2338b28005b7be3db
Cody Rademacher <codyrademacher@Codys-MBP.lan>
2024-01-17 15:58:37 -0600

    Changes made to porfolio project data

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -9,9 +9,9 @@ export default function Resume() {
       <div className="proficiencies">
         <h5>Highlight of my proficiencies:</h5>
         <ul className="proficiencies-list">
-          <li className="proficiencies-item">Front End: HTML, CSS, JavaScript, React.js</li>
+          <li className="proficiencies-item">Front End: HTML, CSS, JavaScript, REACT.js, NEXT.js</li>
           <li className="proficiencies-item">Back End: Express.js, Node.js RESTful API</li>
-          <li className="proficiencies-item">Software Development Tools: Visual Studio Code, Git, GitHub, Heroku</li>
+          <li className="proficiencies-item">Software Development Tools: AWS, Visual Studio Code, Git, GitHub, Heroku</li>
           <li className="proficiencies-item">Database Management: SQL, MongoDB</li>
           <li className="proficiencies-item">Written & Verbal Communication</li>
           <li className="proficiencies-item">Problem Solving and Algorithmic Thinking</li>
