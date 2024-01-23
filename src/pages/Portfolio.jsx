99d1b4c6c5d39a82156898cc68610ae58f1ffb44
Cody Rademacher <codyrademacher@Codys-MBP.lan>
2024-01-23 10:08:40 -0600

    Added project descriptions and changed the styling for mobile

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,24 +1,28 @@
 import Project from "../components/Project";
 import projectData from "../assets/projectData.js";
+import Center from "../components/Center.jsx";
 
 export default function Portfolio() {
   return (
     <div className="portfolio">
       <h1>Portfolio</h1>
-      <div className="container-fluid">
-        <ul className="cards projects">
-          {projectData.map((project) => (
-            <Project
-              key={project.id}
-              imageSrc={project.imageSrc}
-              imageAlt={project.imageAlt}
-              title={project.title}
-              deployedLink={project.deployedLink}
-              githubLink={project.githubLink}
-            />
-          ))}
-        </ul>
-      </div>
+      <Center>
+        <div className="container-fluid">
+          <ul className="cards projects">
+            {projectData.map((project) => (
+              <Project
+                key={project.id}
+                imageSrc={project.imageSrc}
+                imageAlt={project.imageAlt}
+                title={project.title}
+                deployedLink={project.deployedLink}
+                githubLink={project.githubLink}
+                description={project.description}
+              />
+            ))}
+          </ul>
+        </div>
+      </Center>
     </div>
   );
 }
