c70bf89b7dcc0703fd01bb1e80b50dd2e439f1eb
Cody Rademacher <codyrademacher@Codys-MBP.lan>
2024-01-23 12:46:25 -0600

    updated resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,10 @@
 import Button from "../components/Button";
-import MyResume from '../assets/Resume.pdf';
+import MyResume from "../assets/Resume.pdf";
 
 export default function Resume() {
-
-    const openPdf = () => {
-        window.open(MyResume, '_blank');
-    };
-
+  const openPdf = () => {
+    window.open(MyResume, "_blank");
+  };
 
   return (
     <div className="resume-container">
@@ -16,17 +14,32 @@ export default function Resume() {
       <div className="proficiencies">
         <h5>Highlight of my proficiencies:</h5>
         <ul className="proficiencies-list">
-          <li className="proficiencies-item">Front End: HTML, CSS, JavaScript, REACT.js, NEXT.js</li>
-          <li className="proficiencies-item">Back End: Express.js, Node.js RESTful API</li>
-          <li className="proficiencies-item">Software Development Tools: AWS, Visual Studio Code, Git, GitHub, Heroku</li>
-          <li className="proficiencies-item">Database Management: SQL, MongoDB</li>
+          <li className="proficiencies-item">
+            Front End: HTML, CSS, JavaScript, REACT.js, NEXT.js
+          </li>
+          <li className="proficiencies-item">
+            Back End: Express.js, Node.js RESTful API
+          </li>
+          <li className="proficiencies-item">
+            Software Development Tools: AWS, Visual Studio Code, Git, GitHub,
+            Heroku
+          </li>
+          <li className="proficiencies-item">
+            Database Management: SQL, MongoDB
+          </li>
           <li className="proficiencies-item">Written & Verbal Communication</li>
-          <li className="proficiencies-item">Problem Solving and Algorithmic Thinking</li>
+          <li className="proficiencies-item">
+            Problem Solving and Algorithmic Thinking
+          </li>
         </ul>
       </div>
       <div>
         <h4>Download my full resume here!</h4>
-        <Button text="Download PDF" onClick={openPdf} className="resume-button"></Button>
+        <Button
+          text="Download PDF"
+          onClick={openPdf}
+          className="resume-button"
+        ></Button>
       </div>
     </div>
   );
