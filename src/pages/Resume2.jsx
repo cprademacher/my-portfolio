04228b5cf3567154920bf9cffcc02cc67e2af650
Cody Rademacher <cprademacher36@gmail.com>
2024-04-04 14:56:38 -0500

    Added bootcamp certificate to resume page

--- src/pages/Resume2.jsx
+++ src/pages/Resume2.jsx
@@ -1,10 +1,15 @@
 import MyResume from "../assets/Resume.pdf";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import Certificate from "../assets/UT-Bootcamp-Certificate.pdf";
 
 function Resume2() {
   const openPdf = () => {
     window.open(MyResume, "_blank");
+  };
+
+  const openCertificate = () => {
+    window.open(Certificate, "_blank");
   };
 
   return (
@@ -49,6 +54,16 @@ function Resume2() {
             className="bg-success mt-3"
             variant="success"
             onClick={openPdf}
+            size="lg"
+          >
+            My Resume
+          </Button>
+
+          <h5>Download my bootcamp certificate here!</h5>
+          <Button
+            className="bg-success mt-3"
+            variant="success"
+            onClick={openCertificate}
             size="lg"
           >
             My Resume
