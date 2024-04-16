66de350ec13d9acfc47614fe0014ccb663ac9729
Cody Rademacher <cprademacher36@gmail.com>
2024-04-16 11:41:09 -0500

    Added bythescruff website to portfolio

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -2,6 +2,17 @@ const projectData = [
   {
     id: 1,
     imageSrc:
+      "https://codyrad-portfolio.s3.amazonaws.com/ByTheScruff_screenshot.png",
+    imageAlt: "ByTheScruff ECommerce Website screenshot",
+    title: "ByTheScruff ECommerce Website",
+    deployedLink: "https://gorgeous-train-bass.cyclic.app/",
+    githubLink: "https://github.com/cprademacher/by-the-scruff",
+    description:
+      "This ECommerce Website is a cutting-edge eCommerce platform built with REACT.js, offering a seamless shopping experience. Our site integrates Stripe for secure payment processing, utilizes MongoDB for robust data management, and employs Tailwindcss for elegant styling.",
+  },
+  {
+    id: 2,
+    imageSrc:
       "https://codyrad-portfolio.s3.amazonaws.com/Collect-Connect-Screenshot.png",
     imageAlt: "CollectConnect app screenshot",
     title: "CollectConnect",
@@ -11,7 +22,7 @@ const projectData = [
       "COLLECTCONNECT is an application dedicated to collectors. It provides collectors with a platform to curate and display their collections as well as explore collections of other users.  This was a group project built with REACT, and utilizes Bootstrap for styling and responsive design, MongoDB for database, AWS S3 for image storage, ExpressJS/Apollo for the server, and GraphQL for data structuring.",
   },
   {
-    id: 2,
+    id: 3,
     imageSrc:
       "https://codyrad-portfolio.s3.amazonaws.com/which-mountain-screenshot.png",
     imageAlt: "Which Mountain App screenshot",
@@ -20,30 +31,6 @@ const projectData = [
     githubLink: "https://github.com/cprademacher/wm-react",
     description:
       "Welcome to the Ski Mountains in Colorado React app! This application is designed for ski and snowboard enthusiasts and travelers who wish to explore the majestic mountains of Colorado. It provides detailed descriptions of all the ski mountains in the region and offers up-to-date weather forecasts, including current conditions and a 5-day forecast to help make the decision based on snowfall and other conditions. The app fetches data from a reliable weather API, offering users the most current weather information to plan their ski trips effectively.",
-  },
-  {
-    id: 3,
-    imageSrc:
-      "https://codyrad-portfolio.s3.amazonaws.com/ECommerce-Website-Screenshot.png",
-    imageAlt: "ECommerce Website screenshot",
-    title: "ECommerce Website",
-    deployedLink:
-      "https://drive.google.com/file/d/1Mub0PfnQNX7Szr_NwK7oKbNqUkwGWGYF/view",
-    githubLink: "https://github.com/cprademacher/ecommerce-website",
-    description:
-      "This ECommerce Website is a cutting-edge eCommerce platform built with Next.js, offering a seamless shopping experience. Our site integrates Stripe for secure payment processing, utilizes MongoDB for robust data management, leverages Axios for efficient network requests, and employs Styled-Components for elegant styling.",
-  },
-  {
-    id: 4,
-    imageSrc:
-      "https://codyrad-portfolio.s3.amazonaws.com/ECommerce-Admin-Screenshot.png",
-    imageAlt: "ECommerce Admin Page screenshot",
-    title: "ECommerce Admin",
-    deployedLink:
-      "https://drive.google.com/file/d/1Mub0PfnQNX7Szr_NwK7oKbNqUkwGWGYF/view",
-    githubLink: "https://github.com/cprademacher/ecommerce-admin",
-    description:
-      "Welcome to our ECommerce Admin Site, a powerful and efficient management tool for your online store. Built using Next.js, this platform utilizes secure login using Next-Auth, MongoDB for database management, AWS S3 for image storage, Axios for network requests, and Tailwind CSS for styling and responsive design.",
   },
 ];
 
